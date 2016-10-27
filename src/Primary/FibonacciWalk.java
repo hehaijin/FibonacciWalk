52b510e13de0570588227d372c26f8c5d27e1b5f
hehaijin <hehaijin1981@gmail.com>
2016-10-26 21:39:21 -0600

    run()

--- src/Primary/FibonacciWalk.java
+++ src/Primary/FibonacciWalk.java
@@ -2,6 +2,38 @@ package Primary;
 
 public class FibonacciWalk
 {
+  private class WorkerThread extends Thread
+  {
+    private final String NAME; //Name of thread 
+    private long step = 0; //Steps since start
+    private long z; // fib(step) 
+    private long y = 1; // fib(step-1) 
+    private long x = 1; // fib(step-2)
+
+    public WorkerThread(String NAME)
+    {
+      this.NAME=NAME;
+    }
+    
+    public void run()
+    {
+      for(;;)
+      {
+        z=x+y;
+        step++;
+        x=y;
+        y=z;
+      }
+    }
+    
+    
+    
+  }
+   
+  
+  
+  
+  
 
   public static void main(String[] args)
   {
