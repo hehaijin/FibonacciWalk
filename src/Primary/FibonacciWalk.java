010c40087c24267fc144952963beb13b5a08dbf6
hehaijin <hehaijin1981@gmail.com>
2016-10-27 18:08:08 -0600

    added thread isalive() check

--- src/Primary/FibonacciWalk.java
+++ src/Primary/FibonacciWalk.java
@@ -1,5 +1,10 @@
 package Primary;
 
+/**
+ * 
+ * @author Haijin He
+ *
+ */
 public class FibonacciWalk
 {
   private static class WorkerThread extends Thread
@@ -9,12 +14,13 @@ public class FibonacciWalk
     private long z; // fib(step)
     private long y = 1; // fib(step-1)
     private long x = 1; // fib(step-2)
-    private boolean flag = true;//controls the running of the thread.
+    private boolean flag = true;// controls the running of the thread.
 
     public WorkerThread(String NAME)
     {
       this.NAME = NAME;
       z = x + y;
+      step++;
     }
 
     public void run()
@@ -40,13 +46,14 @@ public class FibonacciWalk
         }
 
       }
+      System.out.println(NAME + " is signning out!");
     }
 
     synchronized public void reportStatus()
     {
-      System.out.println("the thread reporting is " + NAME);
-      System.out.println("the step number is " + step);
-      System.out.println("the active numbers are " + x + " " + y + " " + z);
+      System.out.print(NAME);
+      System.out.print(" step:" + step);
+      System.out.print(" the active numbers are: " + x + " " + y + " " + z);
       System.out.println();
     }
 
@@ -64,7 +71,7 @@ public class FibonacciWalk
     WorkerThread th2 = new WorkerThread("worker thread 2");
     th1.start();
     th2.start();
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 10; i++)
     {
       th1.reportStatus();
       th2.reportStatus();
@@ -73,11 +80,22 @@ public class FibonacciWalk
         Thread.sleep(2000);
       } catch (Exception e)
       {
+        System.out.println("something is wrong");
 
       }
     }
+    System.out.println("Now ending the worker threads");
     th1.end();
     th2.end();
+
+    
+    while (th1.isAlive() || th2.isAlive())
+    {
+      System.out.println("Worker threads are not completely stopped!");
+    }
+
+    System.out.println("Program Exit!");
+    System.exit(0);
 
   }
 
